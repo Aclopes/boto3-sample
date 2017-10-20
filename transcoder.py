b6bbb2c598f18f8ab57cc3b75d992484cbb199f3
Alex Lopes <aclopes@gmail.com>
2017-10-20 17:35:44 -0400

    git ignore and space in mais script

--- transcoder.py
+++ transcoder.py
@@ -23,8 +23,24 @@ import boto3
 from botocore.client import ClientError
 
 
+
+
+
+
+
+
+
+
 class AutoTranscodeError(Exception):
     pass
+
+
+
+
+
+
+
+
 
 
 class AutoTranscode(object):
@@ -49,7 +65,7 @@ class AutoTranscode(object):
     more_permissions_policy = {
         'Statement': [
             {
-                'Effect':'Allow',
+                'Effect': 'Allow',
                 'Action': [
                     's3:ListBucket',
                     's3:Put*',
@@ -82,7 +98,7 @@ class AutoTranscode(object):
                     's3:*Delete*',
                     'sns:*Remove*'
                 ],
-                'Resource':'*'
+                'Resource': '*'
             },
         ]
     }
@@ -115,7 +131,16 @@ class AutoTranscode(object):
         'region_name': 'us-west-2',
         'file_pattern': '*.mov'
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def __init__(self, unconverted_directory, converted_directory,
                  in_bucket_name, out_bucket_name,
                  role_name='autotranscode-user',
@@ -125,13 +150,13 @@ class AutoTranscode(object):
                  region_name='us-west-2',
                  file_pattern='*.mov'):
         super(AutoTranscode, self).__init__()
-
+        
         # Local (filesystem) related.
         self.unconverted_directory = unconverted_directory
         self.converted_directory = converted_directory
         self.existing_files = set()
         self.file_pattern = file_pattern
-
+        
         # AWS related.
         self.in_bucket_name = in_bucket_name
         self.out_bucket_name = out_bucket_name
@@ -144,21 +169,30 @@ class AutoTranscode(object):
         self.topic_arn = None
         self.queue_arn = None
         self.pipeline_id = None
-
+        
         self.in_bucket = None
         self.out_bucket = None
         self.role = None
         self.queue = None
-
+        
         # How often should we look at the local FS for updates?
         self.poll_interval = int(poll_interval)
-
+        
         self.s3 = boto3.resource('s3')
         self.iam = boto3.resource('iam')
         self.sns = boto3.resource('sns', self.region_name)
         self.sqs = boto3.resource('sqs', self.region_name)
         self.transcoder = boto3.client('elastictranscoder', self.region_name)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @classmethod
     def load_from_config(cls, config_filepath):
         """
@@ -167,7 +201,16 @@ class AutoTranscode(object):
         with open(config_filepath, 'r') as config:
             config_data = json.load(config)
             return AutoTranscode(**config_data)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @classmethod
     def create_empty_config(cls, config_filepath):
         """
@@ -176,7 +219,16 @@ class AutoTranscode(object):
         """
         with open(config_filepath, 'w') as config:
             json.dump(cls.empty_config_data, config, indent=4)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def ensure_local_setup(self):
         """
         Ensures that the local directory setup is sane by making sure
@@ -186,7 +238,7 @@ class AutoTranscode(object):
             raise AutoTranscodeError(
                 "The unconverted & converted directories can not be the same."
             )
-
+        
         if not os.path.exists(self.unconverted_directory):
             os.makedirs(self.unconverted_directory)
         else:
@@ -194,10 +246,19 @@ class AutoTranscode(object):
             # already been processed. Keep these filenames & only process
             # new ones.
             self.existing_files = set(self.collect_files())
-
+        
         if not os.path.exists(self.converted_directory):
             os.makedirs(self.converted_directory)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def ensure_aws_setup(self):
         """
         Ensures that the AWS services, resources, and policies are set
@@ -209,23 +270,32 @@ class AutoTranscode(object):
         else:
             self.in_bucket = self.s3.create_bucket(
                 Bucket=self.in_bucket_name)
-
+        
         if self.bucket_exists(self.out_bucket_name):
             self.out_bucket = self.s3.Bucket(self.out_bucket_name)
         else:
             self.out_bucket = self.s3.create_bucket(
                 Bucket=self.out_bucket_name)
-
+        
         if self.iam_role_exists():
             self.role = self.iam.Role(self.role_name)
         else:
             self.role = self.setup_iam_role()
-
+        
         self.topic_arn = self.get_sns_topic()
         self.queue = self.get_sqs_queue()
-
+        
         self.pipeline_id = self.get_pipeline()
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def collect_files(self):
         """
         Get a list of all relevant files (based on the file extension)
@@ -233,7 +303,16 @@ class AutoTranscode(object):
         """
         path = os.path.join(self.unconverted_directory, self.file_pattern)
         return glob.glob(path)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def check_unconverted(self):
         """
         Get a list of files that are present in the unconverted directory
@@ -243,14 +322,23 @@ class AutoTranscode(object):
         If no files are present then an empty set is returned.
         """
         current_files = set(self.collect_files())
-
+        
         if not current_files:
             return set()
-
+        
         # Check the new set against the old, returning only new files not found
         # in the old set.
         return current_files.difference(self.existing_files)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def start_converting(self, files_found):
         """
         Upload and convert each file. Uploads are processed in series
@@ -260,17 +348,35 @@ class AutoTranscode(object):
             filename = self.upload_to_s3(filepath)
             self.start_transcode(filename)
             self.existing_files.add(filepath)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def process_completed(self):
         """
         Check the queue and download any completed files from S3 to your
         hard drive.
         """
         to_fetch = self.check_queue()
-
+        
         for s3_file in to_fetch:
             self.download_from_s3(s3_file)
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     # The boto-specific methods.
     def bucket_exists(self, bucket_name):
         """
@@ -282,7 +388,16 @@ class AutoTranscode(object):
             return True
         except ClientError:
             return False
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def iam_role_exists(self):
         """
         Returns ``True`` if an IAM role exists.
@@ -293,7 +408,16 @@ class AutoTranscode(object):
             return True
         except ClientError:
             return None
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def setup_iam_role(self):
         """
         Set up a new IAM role and set its policy to allow Elastic
@@ -305,7 +429,16 @@ class AutoTranscode(object):
         self.iam.RolePolicy(self.role_name, 'more-permissions').put(
             PolicyDocument=json.dumps(self.more_permissions_policy))
         return role
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def get_sns_topic(self):
         """
         Get or create the SNS topic.
@@ -313,7 +446,16 @@ class AutoTranscode(object):
         # Creating a topic is idempotent, so if it already exists
         # then we will just get the topic returned.
         return self.sns.create_topic(Name=self.topic_name).arn
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def get_sqs_queue(self):
         """
         Get or create the SQS queue. If it is created, then it is
@@ -324,7 +466,7 @@ class AutoTranscode(object):
         # then we will just get the queue returned.
         queue = self.sqs.create_queue(QueueName=self.queue_name)
         self.queue_arn = queue.attributes['QueueArn']
-
+        
         # Ensure that we are subscribed to the SNS topic
         subscribed = False
         topic = self.sns.Topic(self.topic_arn)
@@ -332,29 +474,38 @@ class AutoTranscode(object):
             if subscription.attributes['Endpoint'] == self.queue_arn:
                 subscribed = True
                 break
-
+        
         if not subscribed:
             topic.subscribe(Protocol='sqs', Endpoint=self.queue_arn)
-
+        
         # Set up a policy to allow SNS access to the queue
         if 'Policy' in queue.attributes:
             policy = json.loads(queue.attributes['Policy'])
         else:
             policy = {'Version': '2008-10-17'}
-
+        
         if 'Statement' not in policy:
             statement = self.queue_policy_statement
             statement['Resource'] = self.queue_arn
             statement['Condition']['StringLike']['aws:SourceArn'] = \
                 self.topic_arn
             policy['Statement'] = [statement]
-
+            
             queue.set_attributes(Attributes={
                 'Policy': json.dumps(policy)
             })
-
+        
         return queue
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def get_pipeline(self):
         """
         Get or create a pipeline. When creating, it is configured
@@ -366,7 +517,7 @@ class AutoTranscode(object):
             for pipeline in page['Pipelines']:
                 if pipeline['Name'] == self.pipeline_name:
                     return pipeline['Id']
-
+        
         response = self.transcoder.create_pipeline(
             Name=self.pipeline_name,
             InputBucket=self.in_bucket_name,
@@ -378,9 +529,18 @@ class AutoTranscode(object):
                 'Warning': '',
                 'Error': ''
             })
-
+        
         return response['Pipeline']['Id']
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def upload_to_s3(self, filepath):
         """
         Upload a file to the S3 input file bucket.
@@ -390,7 +550,16 @@ class AutoTranscode(object):
             self.in_bucket.Object(filename).put(Body=data)
         print("Uploaded raw video {0}".format(filename))
         return filename
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def start_transcode(self, filename):
         """
         Submit a job to transcode a file by its filename. The
@@ -412,7 +581,16 @@ class AutoTranscode(object):
             }]
         )
         print("Started transcoding {0}".format(filename))
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def check_queue(self):
         """
         Check the queue for completed files and set them to be
@@ -420,28 +598,37 @@ class AutoTranscode(object):
         """
         queue = self.queue
         to_fetch = []
-
+        
         for msg in queue.receive_messages(WaitTimeSeconds=self.poll_interval):
             body = json.loads(msg.body)
             message = body.get('Message', '{}')
             outputs = json.loads(message).get('outputs', [])
-
+            
             if not len(outputs):
                 print("Saw no output in {0}".format(body))
                 continue
-
+            
             key = outputs[0].get('key')
-
+            
             if not key:
                 print("Saw no key in outputs in {0}".format(body))
                 continue
-
+            
             to_fetch.append(key)
             print("Completed {0}".format(key))
             msg.delete()
-
+        
         return to_fetch
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def download_from_s3(self, s3_file):
         """
         Download a file from the S3 output bucket to your hard drive.
@@ -456,11 +643,20 @@ class AutoTranscode(object):
             # loading everything into memory at once.
             for chunk in iter(lambda: body.read(4096), b''):
                 dest.write(chunk)
-
+        
         print("Downloaded {0}".format(destination_path))
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     # End boto-specific methods.
-
+    
     def run(self):
         """
         Start the main loop. This repeatedly checks for new files,
@@ -471,24 +667,32 @@ class AutoTranscode(object):
         # Make sure everything we need is setup, both locally & on AWS.
         self.ensure_local_setup()
         self.ensure_aws_setup()
-
+        
         # Run forever, or until the user says stop.
         while True:
             print("Checking for new files.")
             files_found = self.check_unconverted()
-
+            
             if files_found:
                 print("Found {0} new file(s).".format(len(files_found)))
                 self.start_converting(files_found)
-
+            
             # Here we check the queue, which will long-poll
             # for up to ``self.poll_interval`` seconds.
             self.process_completed()
 
 
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     import sys
-
+    
     config_filepath = os.path.abspath(
         os.path.expanduser(
             '~/.autotranscode.json'
@@ -501,10 +705,10 @@ if __name__ == '__main__':
         print("Created an empty config file at %s." % config_filepath)
         print("Please modify it & re-run this command.")
         sys.exit(1)
-
+    
     # If so, load from it & run.
     auto = AutoTranscode.load_from_config(config_filepath)
-
+    
     try:
         auto.run()
     except KeyboardInterrupt:
